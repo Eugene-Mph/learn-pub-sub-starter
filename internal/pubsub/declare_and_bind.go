c1f48dff6b21d7fe46aa2e40731bd733b60f6f29
Eugene-Mph <eugenemphathiwa13@gmail.com>
2025-11-30 09:40:15 +0200

    add client queue

---
+++ internal/pubsub/declare_and_bind.go
@@ -0,0 +1,51 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueTransient SimpleQueueType = iota // 0
+	SimpleQueueDurable                          // 1
+)
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+
+	Ch, err := conn.Channel()
+
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not create channel: %v", err)
+	}
+
+	queue, err := Ch.QueueDeclare(
+		queueName,
+		queueType != SimpleQueueTransient, // durable
+		queueType == SimpleQueueTransient, // autoDelete
+		queueType == SimpleQueueTransient, // exclusive
+		false,                             // no-wait
+		nil,                               // args
+	)
+
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	err = Ch.QueueBind(
+		queue.Name, // queue name
+		key,        // routing key
+		exchange,   // exchange
+		false,      // no-wait
+		nil,        // args
+	)
+
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+
+	return Ch, queue, nil
+
+}
